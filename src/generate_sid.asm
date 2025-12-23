b032bb2e509d8c8889c65d3b63ad39c9717f7554
St0fF-NPL-ToM <st0ff.npl@gmail.com>
2025-12-23 23:27:23 +0100

    fix by Sonic: sidPlay2 needs PSIDv3 with dual SID
    also, filled meta data

--- src/generate_sid.asm
+++ src/generate_sid.asm
@@ -7,15 +7,15 @@
 
 .macro PSID_header( loadAddr, initAddr, playAdr, sid2, sid3 ) {
 	.dword flipEndian4( $50534944 )	// PSID
-	.word flipEndian2( 4 )				// Version 4
+	.word flipEndian2( 3 )				// Version 4
 	.word flipEndian2( $7c )			// dataOffset, version 2+
 	.word flipEndian2( loadAddr ), flipEndian2( initAddr ), flipEndian2( playAdr )
 	.word flipEndian2( 1 ),flipEndian2( 1 )
 	.dword 0
 	.encoding "ascii"
-	.text "                                "
-	.text "                                "
-	.text "                                "
+	.text "TinyDancer: identifying variable"	//
+	.text "St0fF / the0bsessedManiacs ^ NPL"	//
+	.text "2025-12-24                      "	//
 	.word flipEndian2( %0000000110 )	// flags
 	.byte 0,0			// relocStartPage, relocPages
 	.byte sid2, sid3
