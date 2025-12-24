e89ed7711b45f47b6ffe6d4d3de1e349feb5b472
St0fF-NPL-ToM <st0ff.npl@gmail.com>
2025-12-24 16:18:22 +0100

    added file id, fixed .sid generation
    Thank you iAN CooG for dipping me onto this:
    - created macro "createPSIDsingle"
    - added handling HVSC filename restrictions
    - finalized the macro to directly output a .sid file.

--- src/generate_sid.asm
+++ src/generate_sid.asm
@@ -1,28 +1,69 @@
 /*
  *	Trying to make KickAssembler output a valid .sid file.
  *	======================================================
- *	-> 2 SIDs, Addresses $D400, $D420
- *	-> new/old egal.
+ *		St0fF/NPL^tOM'25
  */
-
-.macro PSID_header( loadAddr, initAddr, playAdr, sid2, sid3 ) {
-	.dword flipEndian4( $50534944 )			// PSID
-	.word flipEndian2( sid3 > 0 ? 4 : 3 )	// Version selector
-	.word flipEndian2( $7c )				// dataOffset, version 2+
-	.word flipEndian2( loadAddr ), flipEndian2( initAddr ), flipEndian2( playAdr )
-	.word flipEndian2( 1 ),flipEndian2( 1 )
-	.dword 0
+.enum { SID_unknown, SID_6581, SID_8580, SID_BOTH }
+.enum { video_unknown, PAL, NTSC, video_both }
+// This macro is not yet finished, but working for my files at hand.
+// I actually do not understand the idea of requesting multiple different chips,
+// so I only provide one SID chip selector.
+.macro createPSIDsingle( title, author, reldate, sid2, sid3, load, init, play, segment, sidtype, video_std ) {
+	// Replace bad title characters by underscores
+	.const badC = @" :;,./\\|()[]{}<>«»\"'`!?+=*"
+	.var lastU = false
+	.var fn = ""
+	.for ( var i=0; i < title.size(); i++ ) {
+		.var j=0;
+		.while ( j < badC.size() && badC.charAt( j ) != title.charAt( i ) ) .eval j++
+		.if ( j < badC.size() ) {
+			.if ( !lastU ) {
+				.eval lastU = true
+				.eval fn += "_"
+			}
+		} else {
+			.eval fn += title.charAt( i )
+			.eval lastU = false
+		}
+	}
+	.if ( lastU ) .eval fn = fn.substring( 0, fn.size() - 1 )
+	// Filename part derived from title.
+	.if ( sid2 == 0 )		.eval fn += "_PSID.sid"
+	else .if ( sid3 == 0 )	.eval fn += "_2SID.sid"
+	else 					.eval fn += "_3SID.sid"
+	// Filename finalized ( I hope ... ) tell KickAssembler to produce output!
+	.segment PSID_file [outBin=fn]
+	// Create PSid header
+	.dword flipEndian4( $50534944 )			// 	PSID
+	.word flipEndian2( sid2 > 0 ? sid3 > 0 ? 4 : 3 : 2 )// Version selector
+	.word flipEndian2( $7c )				// 	dataOffset, version 2+
+	.word flipEndian2( load ), flipEndian2( init ), flipEndian2( play )
+	.word flipEndian2( 1 ),flipEndian2( 0 )	// 	Single Song.
+	.dword 0								//	VBlank
 	.encoding "ascii"
-	.text "TinyDancer: »identify variable« "	//
-	.text "St0fF / theObsessedManiacs ^ NPL"	//
-	.text "2025-12-24                      "	//
-	.word flipEndian2( %0000000110 )	// flags
+	makePSIDtx( title )
+	makePSIDtx( author )
+	makePSIDtx( reldate )
+	// create Flags:	starting with "built-in player", v2/3/4, and adding timing info:
+	.var flg = %10 | ( video_std << 2 )
+	.eval flg = flg | ( sidtype << 4 )
+	.if ( sid2 > 0 ) .eval flg = flg | ( sidtype << 6 )
+	.if ( sid3 > 0 ) .eval flg = flg | ( sidtype << 8 )
+	.word flipEndian2( flg )	// flags
 	.byte 0,0			// relocStartPage, relocPages
-	.byte sid2, sid3
+	.byte ( sid2 >> 4 ) & $ff, ( sid3 >> 4 ) & $ff
+	.segmentout [segments=segment]
 }
 .function flipEndian4( x ) {
 	.return ((x & $ff000000) >> 24) | ((x & $ff0000) >> 8) | ((x & $ff00) << 8) | ((x & $ff) << 24)
 }
 .function flipEndian2( x ) {
 	.return ((x & $ff) << 8) | (( x & $ff00 ) >> 8)
+}
+.macro makePSIDtx( txt ) {
+	.var sl = txt.size()
+	.var lst = List()
+	.for ( var i=0 ; i < sl; i++ ) .eval lst.add( txt.charAt( i ) )
+	.for ( ; sl < 32; sl++ ) .eval lst.add( 0 )
+	.fill lst.size(), lst.get( i )
 }
--- src/main.asm
+++ src/main.asm
@@ -1,9 +1,12 @@
 
 .segmentdef Header
 
-#import "generate_sid.asm"
 #import "player.asm"
+#import "generate_sid.asm"
+.encoding "ascii"
 
-.segment PSID_file [outBin="tinydancer-identify_variable.sid"]
-PSID_header( $1000, $1000, $1003, $42, 0 )
-.segmentout [segments="MusicPlayer"]
+.const title 	=	"TinyDancer: »identify variable«"
+.const author	=	"St0fF / theObsessedManiacs ^ NPL"
+.const reldate	=	"2025-12-24"
+
+createPSIDsingle( title, author, reldate, $d420, 0, init, init, play, "MusicPlayer", SID_unknown, PAL )
